74a333ccf492d94ddf82e24a672ffc3f7e4c7940
unknown <fuaad29@gmail.com>
2024-04-15 22:46:58 +0500

    code

---
+++ data.js
@@ -0,0 +1,2 @@
+const data = {name: "Muhammad Fuaad", email: "fuaad29@gmail.com", profession: "Web Developer"} 
+module.exports = data
---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require("http")
+const data = require("./data.js")
+http.createServer((request, response)=>{
+  response.writeHead(200, {"content_type": "application/json"})
+  response.write(JSON.stringify(data))
+  response.end()
+}).listen(5000)
