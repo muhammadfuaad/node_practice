f2cc62cacf0a9ec210da405ae5849b34f8ffc894
unknown <fuaad29@gmail.com>
2024-04-23 23:36:22 +0500

    added new operation through mongoose

--- api.js
+++ api.js
@@ -11,33 +11,29 @@ app.get("/", async (req, res)=>{
 
 app.post("/", async (req, res)=>{
   // console.log(req.body);
-  const db = await dbConnection()
+  const data = await dbConnection()
   // console.log(req.body);
   // console.log(req.body.length);
   let result
   if (req.body.length > 1) {
-    result = await db.insertMany(req.body)
+    result = await data.insertMany(req.body)
   } else {
-    result = await db.insertOne(req.body)
+    result = await data.insertOne(req.body)
   }
   console.log(result);
 })
 
-app.put("/", async (req, res)=>{
+app.put("/:name", async (req, res)=>{
   // console.log(req.body);
-  const db = await dbConnection()
-  const result = await db.updateOne({name: req.body.name}, {$set: req.body})
+  const data = await dbConnection()
+  const result = await data.updateOne({name: req.params.name}, {$set: req.body})
   console.log("req.body:", req.body);
 res.send({result: "update"})})
 
 app.delete("/", async (req, res)=>{
-  const db = await dbConnection()
-  let result
-  if (req.body.length > 1) {
-    result = await db.deleteMany(req.body)
-  } else {
-    result = await db.deleteOne(req.body)
-  }
+  const data = await dbConnection()
+  let result = await data.deleteMany(req.body) || await data.deleteOne(req.body) 
+  
   console.log("result:", result);
 })
 
---
+++ index_7.js
@@ -0,0 +1,28 @@
+const express = require("express")
+require("./config")
+const User = require("./users")
+
+
+const app = express()
+app.use(express.json())
+
+app.post("/create", async (req, res)=>{
+  let data = new User(req.body)
+  let result =await data.save()
+  console.log(result);
+  res.send(result)
+})
+
+app.get("/list", async (req, res)=>{
+  let data = await User.find()
+  res.send(data)
+})
+
+app.delete("/delete/:_id", async (req, res)=>{
+  console.log(req.params);
+  let data = await User.deleteOne(req.params)
+  res.send(data)
+})
+
+
+app.listen(5000)
