c0622d284c4fae2a7a088af519d965b8ae41ed7a
unknown <fuaad29@gmail.com>
2024-04-24 22:15:52 +0500

    added sendData function in index_4.js

--- index_4.js
+++ index_4.js
@@ -1,14 +1,24 @@
 const {MongoClient} = require("mongodb")
 const url = "mongodb+srv://fuaad29:1234567890@cluster0.imqnzjb.mongodb.net/"
 const datbaseName = "ecommerce"
+const collectionName = "users"
 const client = new MongoClient(url)
-
-async function getData() {
+async function crud() {
   let result = await client.connect()
   db = result.db(datbaseName)
-  collection = db.collection("users")
-  let data = await collection.find({name: "P3"}).toArray()
-  console.log(data);
-}
+  collection = db.collection(collectionName)
+  async function getData() {
+    let data = await collection.find({name: "Mehsum"}).toArray()
+    console.log("data:", data);
+  }
+  getData()
 
-getData()
+  async function sendData() {
+    console.log("This is sendData");
+    let data = {name: "Burhan"}
+    let result = await collection.insertOne(data)
+    console.log("result:", result);
+  }
+  sendData()
+}
+crud()
