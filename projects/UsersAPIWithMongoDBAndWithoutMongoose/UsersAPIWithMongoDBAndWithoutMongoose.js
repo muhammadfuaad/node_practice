a5a8b9f937b508c832cb978984c1bc407c722729
unknown <fuaad29@gmail.com>
2024-06-07 21:31:46 +0500

    code

--- projects/UsersAPIWithMongoDBAndWithoutMongoose/UsersAPIWithMongoDBAndWithoutMongoose.js
+++ projects/UsersAPIWithMongoDBAndWithoutMongoose/UsersAPIWithMongoDBAndWithoutMongoose.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const mongodb = require("mongodb")
 const cors = require('cors')
 const PORT = 3000
-const ObjectId = mongodb.ObjectId;
+const { ObjectId } = require('mongodb');
+// const mongodb = require("mongodb")
 
 const app = express()
 app.use(express.json())
@@ -17,22 +17,43 @@ app.get("/users", async(req, res)=>{
   res.send(data)
 })
 
-app.post("/create_user", async(req, res)=>{
+app.post("/create", async(req, res)=>{
   const usersCollection = await dbConnection()
   const data = req.body
-  // console.log(req.body);
   let result = await usersCollection.insertOne(data)
   res.send(result)
 })
 
-app.delete("/delete_user/:id", async(req, res)=>{
+app.delete("/delete/:id", async(req, res)=>{
   const usersCollection = await dbConnection()
-  const id = req.params
-  const deleteObject = await usersCollection.find({id: new ObjectId(id)})
+  const { id } = req.params;
+  const objectId = new ObjectId(id);
+  const deleteObject = await usersCollection.findOne({_id: objectId})
   console.log("deleteObject:", deleteObject);
-  const result = await usersCollection.deleteOne({id: new ObjectId(id)})
+  const result = await usersCollection.deleteOne({_id: objectId})
   res.send(result)
   // res.send(`${result}, ${id}, ${deleteObject}`)
+  // question:
+  // Why does it behave differently?
+
+})
+
+app.put("/edit/:id", async(req, res)=>{
+  const usersCollection = await dbConnection()
+  const { id } = req.params;
+  const objectId = new ObjectId(id);
+  const updatedKey = Object.keys(req.body)[0]
+  const updatedValue = Object.values(req.body)[0]
+  const updateObject = await usersCollection.findOne({_id: objectId})
+  console.log("updateObject:", updateObject);
+  console.log("objectId:", objectId);
+  console.log("typeof objectId:", typeof objectId);
+
+  const result = await usersCollection.updateOne({_id: objectId},{$set: {[updatedKey]: updatedValue}})
+  // question:
+  // Why do we need to use [updatedKey]?
+  
+  res.send(result)
 })
 app.listen(PORT, ()=>{
   console.log(`server is runnng at ${PORT}`);
